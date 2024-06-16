ac62086836f7374af81024cbd4c74f703240533b
frostice482 <frostice48260@gmail.com>
2024-06-16 15:51:54 +0700

    add loader type for `onLoad` return

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,33 @@ import esbuild = require("esbuild");
 import fs = require("fs");
 import { compile as sCompile } from "surplus/compiler";
 
-const tsxOpts: esbuild.TransformOptions = { loader: "tsx", jsx: "preserve" };
-
 export const surplusEsbuild: esbuild.Plugin = {
     name: "surplus-esbuild",
     setup(build) {
+        build.initialOptions.sourcemap
         // for .jsx files
         build.onLoad({ filter: /\.jsx$/i }, async ({ path }) => {
             const content = await fs.promises.readFile(path, 'utf8');
             const compiled = sCompile(content) as string;
 
-            return { contents: compiled };
+            return {
+                contents: compiled,
+                loader: 'js'
+            };
         });
 
         // for .tsx files
         build.onLoad({ filter: /\.tsx$/i }, async ({ path }) => {
             const content = await fs.promises.readFile(path, 'utf8');
-            const transformed = await esbuild.transform(content, tsxOpts);
+            const transformed = await esbuild.transform(content, {
+                loader: 'tsx',
+                jsx: 'preserve',
+                jsxFactory: 'Surplus'     
+            });
             const compiled = sCompile(transformed.code) as string;
 
             return {
+                loader: 'js',
                 contents: compiled,
                 warnings: transformed.warnings,
             };
